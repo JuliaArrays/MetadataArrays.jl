c6b3d7acdcb72c4b23d3ee0b9729df14b8550896
Pietro Vertechi <pietro.vertechi@protonmail.com>
2019-04-17 23:21:31 +0100

    update to latest julia (#2)
    * update to julia 1
    
    * fix travis

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,4 @@
-using MetadataArrays
-@static if VERSION < v"0.7.0-DEV.2005"
-    using Base.Test
-else
-    using Test
-end
+using MetadataArrays, Test
 
 @testset "MetadataVector" begin
     v = [1, 3, 5, 4]

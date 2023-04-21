80f83c407a6c2458aa3269d692e015723a490d06
Zachary P. Christensen <zchristensen7@gmail.com>
2023-04-21 02:35:26 -0400

    mutable metadata api methods

--- src/MetadataArrays.jl
+++ src/MetadataArrays.jl
@@ -1,10 +1,11 @@
 module MetadataArrays
 
-import ArrayInterfaceCore: parent_type, is_forwarding_wrapper, can_setindex,
+import ArrayInterface: parent_type, is_forwarding_wrapper, can_setindex,
     can_change_size
 using Base: BroadcastStyle
 using DataAPI
-import DataAPI: metadata, metadatakeys, metadatasupport
+import DataAPI: metadata, metadata!, metadatakeys, metadatasupport, deletemetadata!,
+    emptymetadata!
 using LinearAlgebra
 
 export
@@ -20,7 +21,9 @@ struct MetadataArray{T, N, P<:AbstractArray,M<:NamedTuple} <: AbstractArray{T, N
     parent::P
     metadata::M
 
-    global _MDArray(p, md) = new{eltype(p),ndims(p),typeof(p),typeof(md)}(p, md)
+    global function _MDArray(p, md)
+        new{eltype(p), ndims(p), typeof(p), typeof(md)}(p, md)
+    end
 end
 
 """
@@ -79,14 +82,13 @@ function Base.convert(T::Type{<:MetadataArray}, mda::MetadataArray)
     else
         MetadataArray(
             convert(parent_type(T), parent(mda)),
-            convert(fieldtype(T, :metadata), getfield(mda, :metadata))
+            convert(fieldtype(T, :metadata), metadata(mda))
         )
     end
 end
 
 Base.parent(mda::MetadataArray) = getfield(mda, :parent)
 parent_type(T::Type{<:MetadataArray}) = fieldtype(T, :parent)
-
 
 is_forwarding_wrapper(T::Type{<:MetadataArray}) = true
 can_setindex(T::Type{<:MetadataArray}) = can_setindex(parent_type(T))
@@ -107,6 +109,7 @@ Base.strides(mda::MetadataArray) = strides(parent(mda))
 Base.length(mda::MetadataArray) = length(parent(mda))
 Base.firstindex(mda::MetadataArray) = firstindex(parent(mda))
 Base.lastindex(mda::MetadataArray) = lastindex(parent(mda))
+
 Base.pointer(mda::MetadataArray) = pointer(parent(mda))
 
 Base.in(val, mda::MetadataArray) = in(val, parent(mda))
@@ -114,7 +117,13 @@ Base.sizehint!(mda::MetadataArray, n::Integer) = sizehint!(parent(mda), n)
 Base.keys(mda::MetadataArray) = keys(parent(mda))
 Base.isempty(mda::MetadataArray) = isempty(parent(mda))
 
-Base.dataids(mda::MetadataArray) = (Base.dataids(parent(mda))..., Base.dataids(getfield(mda, :metadata))...)
+Base.@propagate_inbounds function Base.isassigned(mda::MetadataArray, i::Integer...)
+    isassigned(parent(mda), i...)
+end
+
+function Base.dataids(mda::MetadataArray)
+    (Base.dataids(parent(mda))..., Base.dataids(getfield(mda, :metadata))...)
+end
 
 Base.propertynames(mda::MetadataArray) = keys(getfield(mda, :metadata))
 Base.getproperty(mda::MetadataArray, s::Symbol) = getfield(getfield(mda, :metadata), s)
@@ -122,6 +131,7 @@ Base.getproperty(mda::MetadataArray, s::String) = getproperty(mda, Symbol(s))
 Base.hasproperty(mda::MetadataArray, s::Symbol) = haskey(getfield(mda, :metadata), s)
 Base.hasproperty(mda::MetadataArray, s::String) = hasproperty(mda, Symbol(s))
 
+metadata(mda::MetadataArray) = getfield(mda, :metadata)
 function metadata(mda::MetadataArray, key::AbstractString; style=nothing)
     metadata(mda, Symbol(key); style=style)
 end
@@ -136,20 +146,34 @@ end
 end
 _metadata(md, ::Nothing) = md
 _metadata(md, style::Bool) = style ? (md, :default) : md
+
 metadatakeys(mda::MetadataArray) = propertynames(mda)
 metadatasupport(T::Type{<:MetadataArray}) = (read=true, write=false)
+
+deletemetadata!(mda::MetadataArray, key) = delete!(metadata(mda), key)
+
+emptymetadata!(mda::MetadataArray) = empty!(metadata(mda))
+
 dropmeta(mda::MetadataArray) = parent(mda)
 dropmeta(x) = x
+
+# TODO function metadata! end
+
+Base.write(io::IO, mda::MetadataArray) = write(io, parent(mda))
+function Base.read!(io::IO, mda::MetadataArray)
+    read!(io, parent(mda))
+    return mda
+end
 
 @specialize
 @inline function Base.similar(mda::MetadataArray)
     _MDArray(similar(parent(mda)), getfield(mda, :metadata))
 end
 @inline function Base.similar(mda::MetadataArray, ::Type{T}) where {T}
-    _MDArray(similar(parent(mda), T), getfield(mda, :metadata))
+    _MDArray(similar(parent(mda), T), metadata(mda))
 end
 function Base.similar(mda::MetadataArray, ::Type{T}, dims::Dims) where {T}
-    _MDArray(similar(parent(mda), T, dims), getfield(mda, :metadata))
+    _MDArray(similar(parent(mda), T, dims), metadata(mda))
 end
 
 function Base.reshape(s::MetadataArray, d::Dims)
